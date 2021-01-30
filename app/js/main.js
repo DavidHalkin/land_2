e9fabf0bf079997d77133edbb46f2b531765625f
David_Halkin <northfire@mail.ru>
2021-01-30 17:53:01 +0200

    sdf

--- app/js/main.js
+++ app/js/main.js
@@ -1,21 +1,4 @@
-// $(document).ready(function(){
-// 	$('.proposition_gal_js').owlCarousel({
-// 	    responsiveClass:true,
-// 	    responsive:{
-// 	        0:{
-// 	            items:2,
-// 	            center: true,
-// 	            nav:false
-// 	        },
-// 	        520:{
-// 	            items:3,
-// 	            nav:false
-// 	        }
-// 	    }
-// 	})
-// });
 function CarouselInit() {
-
    var checkWidth = $(window).width();
    var carousel = $(".proposition_gal_js");
    if (checkWidth < 575) {
@@ -36,3 +19,27 @@ function CarouselInit() {
 };
 $(document).ready(CarouselInit);
 $(window).resize(CarouselInit);
+
+$(document).ready(function(){
+  $(".more_info_js .more_info_btn").click(function(){
+    $(this).toggleClass("active");
+    $(this).parent().find(".more_info_content").slideToggle("fast");
+  });
+  // tooltip
+    var tooltipTemplate = ['<div class="tooltip tooltip_custom" role="tooltip">',
+      '<div class="arrow"></div>',
+      '<div class="tooltip-inner text-left font-weight-bold px-3 py-2">',
+      '</div>',
+      '</div>'].join('');
+      // $('[data-toggle="tooltip_custom"]').tooltip("show")
+    $('[data-toggle="tooltip_custom"]').tooltip({
+      template: tooltipTemplate
+    });
+
+  $('.mini_gal_js').owlCarousel({
+    items:1,
+    nav:true,
+    loop:true,
+    navText:["<svg width='24' height='24' viewBox='0 0 24 24'  xmlns='http://www.w3.org/2000/svg'><g fill='none'><path style='stroke: rgb(255, 255, 255);' stroke-width='2' d='M5 8l7 7 7-7'></path></g></svg>","<svg width='24' height='24' viewBox='0 0 24 24'  xmlns='http://www.w3.org/2000/svg'><g fill='none'><path style='stroke: rgb(255, 255, 255);' stroke-width='2' d='M5 8l7 7 7-7'></path></g></svg>"],
+  });
+});
